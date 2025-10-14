4957efe4ca88fd0ffe0bdc63ef29bb8efec5bcdc
Nina Moore <nsm2004@Ninas-MacBook-Air-2.local>
2025-10-14 15:28:19 -0500

    SCRUM-33 Create homepage- nina

---
+++ navisewebsite/src/main/java/com/example/navisewebsite/NavisewebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.navisewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavisewebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavisewebsiteApplication.class, args);
+	}
+
+}
---
+++ navisewebsite/src/test/java/com/example/navisewebsite/NavisewebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.navisewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavisewebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
